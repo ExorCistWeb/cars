5bbb510259a35dc55ae5bf8be4ff0dc0f99e3b07
ExorCistWeb <75250883+ExorCistWeb@users.noreply.github.com>
2024-09-27 02:30:51 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,3 +3,13 @@ var swiper = new Swiper(".mySwiper", {
         el: ".swiper-pagination",
     },
 });
+
+$(document).ready(function() {
+    $(".faq-question").click(function() {
+        $(this).toggleClass("active");
+        $(this).next(".faq-answer").slideToggle(300);
+        // Переключение иконки
+        var icon = $(this).find(".toggle-icon");
+
+    });
+});
